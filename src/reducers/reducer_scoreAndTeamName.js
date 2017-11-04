62d91297ce83d401c01f33144c08a464edc5ef3c
randomlysa <su@randomlysa.com>
2017-11-04 12:02:20 -0500

    save names, score to localstorage

--- src/reducers/reducer_scoreAndTeamName.js
+++ src/reducers/reducer_scoreAndTeamName.js
@@ -3,37 +3,54 @@ import {
     UPDATE_TEAM_NAME
 } from '../actions/index';
 
-const initialState = {
-    homeTeamName: 'Home Team',
-    awayTeamName: 'Away Team',
-    homeScore: 0,
-    awayScore: 0
+import { loadState, saveState } from '../manageLocalStorage';
+
+let initialState;
+
+// No state in local storage, use default.
+if (!loadState()) {
+    initialState = {
+        homeTeamName: 'Home Team',
+        awayTeamName: 'Away Team',
+        homeScore: 0,
+        awayScore: 0
+    }
+// Otherwise use state from local storage.
+} else {
+    initialState = loadState();
 }
 
 export default function(state = initialState, action) {
+    let newState;
+
     switch (action.type) {
         case UPDATE_SCORE:
-
             let homeScore = state.homeScore;
             let awayScore = state.awayScore;
 
             if (action.payload.teamId === "home") {
                 let newHomeScore = homeScore + parseInt(action.payload.score);
-                return { ...state, homeScore: newHomeScore };
+                newState = { ...state, homeScore: newHomeScore };
             }
 
             if (action.payload.teamId === "away") {
                 let newAwayScore = awayScore + parseInt(action.payload.score);
-                return { ...state, awayScore: newAwayScore };
+                newState = { ...state, awayScore: newAwayScore };
             }
+
+            saveState(newState)
+            return newState;
 
         case UPDATE_TEAM_NAME:
             if (action.payload.teamId === "home") {
-                return { ...state, homeTeamName: action.payload.newTeamName }
+                newState = { ...state, homeTeamName: action.payload.newTeamName }
             }
             if (action.payload.teamId === "away") {
-                return { ...state, awayTeamName: action.payload.newTeamName }
+                newState = { ...state, awayTeamName: action.payload.newTeamName }
             }
+
+            saveState(newState)
+            return newState;
 
         default:
             return state
