99f16056f41c36ff3d2fbaa4ecf0c2ef79642d71
randomlysa <su@randomlysa.com>
2017-11-04 00:58:55 -0500

    update indents

--- src/components/teamAndscore.js
+++ src/components/teamAndscore.js
@@ -7,60 +7,59 @@ import * as actionCreators from '../actions'
 class TeamAndScore extends Component {
 
     componentDidMount() {
-        }
-
+    }
 
     render(props) {
 
-    let teamName = this.props.teamName;
-    let teamId = this.props.teamId;
+        let teamName = this.props.teamName;
+        let teamId = this.props.teamId;
 
-    let score;
-    if (teamId === 'home') {
-        score = this.props.score[0];
-    } else {
-        score = this.props.score[1];
+        let score;
+        if (teamId === 'home') {
+            score = this.props.score[0];
+        } else {
+            score = this.props.score[1];
+        }
+
+        return (
+            <div>
+                <h2>{teamName}</h2>
+                <h2>Score: {score}</h2>
+
+                <button
+                    onClick={this.props.actions.updateScore.bind(this, teamId, "+2")}
+                >
+                    +2
+                </button>
+
+                <button
+                    onClick={this.props.actions.updateScore.bind(this, teamId, "+3")}
+                >
+                    +3
+                </button>
+
+                <button
+                    onClick={this.props.actions.updateScore.bind(this, teamId, "+1")}
+                >
+                    +1
+                </button>
+
+                <br />
+
+                <button
+                    onClick={this.props.actions.updateScore.bind(this, teamId, "-1")}
+                >
+                    -1
+                </button>
+
+                <button
+                    onClick={this.props.actions.updateScore.bind(this, teamId, "-2")}
+                >
+                    -2
+                </button>
+            </div>
+        );
     }
-
-    return (
-    <div>
-        <h2>{teamName}</h2>
-        <h2>Score: {score}</h2>
-
-        <button
-            onClick={this.props.actions.updateScore.bind(this, teamId, "+2")}
-        >
-            +2
-        </button>
-
-        <button
-            onClick={this.props.actions.updateScore.bind(this, teamId, "+3")}
-        >
-            +3
-        </button>
-
-        <button
-            onClick={this.props.actions.updateScore.bind(this, teamId, "+1")}
-        >
-            +1
-        </button>
-
-        <br />
-
-        <button
-            onClick={this.props.actions.updateScore.bind(this, teamId, "-1")}
-        >
-            -1
-        </button>
-
-        <button
-            onClick={this.props.actions.updateScore.bind(this, teamId, "-2")}
-        >
-            -2
-        </button>
-    </div>
-    );
-  }
 }
 
 // Gets info from state.
