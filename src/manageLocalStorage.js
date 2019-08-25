453c49bb58ec80b26f6eb9e8b2c1eecf02c18ee0
Sasha A <su@randomlysa.com>
2019-08-24 23:04:23 -0500

    run prettier
    no changes to code, only formatting

--- src/manageLocalStorage.js
+++ src/manageLocalStorage.js
@@ -1,31 +1,31 @@
 // https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage
 
 export const loadState = () => {
-    try {
-        const serializedState = localStorage.getItem('score');
-        if (serializedState === null) {
-            return undefined;
-        }
-        return JSON.parse(serializedState)
-    } catch (err) {
-        return undefined;
+  try {
+    const serializedState = localStorage.getItem("score");
+    if (serializedState === null) {
+      return undefined;
     }
-}
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
 
-export const saveState = (score) => {
-    try {
-        const serializedState = JSON.stringify(score);
-        localStorage.setItem('score', serializedState);
-    } catch (err) {
-        // Ignore write errors.
-    }
-}
+export const saveState = score => {
+  try {
+    const serializedState = JSON.stringify(score);
+    localStorage.setItem("score", serializedState);
+  } catch (err) {
+    // Ignore write errors.
+  }
+};
 
 export const clearStorage = () => {
-    try {
-        localStorage.setItem('score', '');
-        window.location.reload(true);
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    localStorage.setItem("score", "");
+    window.location.reload(true);
+  } catch (err) {
+    console.log(err);
+  }
+};
