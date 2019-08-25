0f5bd53940fc6d0034b48c8e5494773393b46d12
Sasha A <su@randomlysa.com>
2019-08-24 23:46:09 -0500

    run prettier

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,30 @@
-export const UPDATE_SCORE = 'UPDATE_SCORE';
-export const UPDATE_TEAM_NAME = 'UPDATE_TEAM_NAME';
-export const UNAUTH_TWITTER = 'UNAUTH_TWITTER';
+export const UPDATE_SCORE = "UPDATE_SCORE";
+export const UPDATE_TEAM_NAME = "UPDATE_TEAM_NAME";
+export const UNAUTH_TWITTER = "UNAUTH_TWITTER";
 
 export function updateScore(teamId, scoreUpdate) {
-    return {
-        type: UPDATE_SCORE,
-        payload: {
-            teamId: teamId,
-            score: scoreUpdate
-        }
+  return {
+    type: UPDATE_SCORE,
+    payload: {
+      teamId: teamId,
+      score: scoreUpdate
     }
+  };
 }
 
 export function updateTeamName(teamId, newTeamName) {
-    console.log(teamId, newTeamName)
-    return {
-        type: UPDATE_TEAM_NAME,
-        payload: {
-            teamId: teamId,
-            newTeamName: newTeamName
-        }
+  console.log(teamId, newTeamName);
+  return {
+    type: UPDATE_TEAM_NAME,
+    payload: {
+      teamId: teamId,
+      newTeamName: newTeamName
     }
+  };
 }
 
 export function unauthTwitter() {
-    return {
-        type: UNAUTH_TWITTER
-    }
+  return {
+    type: UNAUTH_TWITTER
+  };
 }
